c371e6661d88332709b8407a1d126d56b9e6a6ad
Daniyel4 <tegamoses1@gmail.com>
2024-12-20 01:17:14 +0100

    new file:   src/App/Config/Middleware.php 	new file:   src/App/Config/Paths.php 	new file:   src/Framework/Validator.php 	phpiggy/

---
+++ src/App/Controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controllers; 
+
+use Framework\TemplateEngine;
+use App\Config\Paths;
+use App\Services\TransactionService;
+
+class HomeController{
+
+
+
+
+    public function __construct(
+      
+      private TemplateEngine $view,
+      private TransactionService $transactionService
+      ){
+       
+    }
+
+
+        public function home(){
+
+
+          $transactions = $this->transactionService->getUserTransactions();
+            
+          echo  $this->view->render("/index.php", [
+
+            'transactions' => $transactions
+
+          ]);
+
+           
+        }
+
+
+
+}
